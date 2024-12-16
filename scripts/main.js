428b3f92efbc8243bbd53789da4ef7a88dc78174
Xtopuknul <xto.puknul@gmail.com>
2024-12-16 21:00:40 +0100

    message

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+document.querySelector("img").onclick = function () {
+  alert ("ААА ТЫ НЕ ПРЕДСТАВЛЯЕШЬ НА СКОЛЬКО ЭТО БОЛЬНО");
+}
+
+
+// Используй .map() для создания массива с удвоенными значениями исходного массива.
+// var pops = [1, 2, 4, 5, 1];
+// for (let i = 0; i <pops.length; i++) {
+//   var cocker = pops.map (num => num ** 30)
+// }
+// console.log(cocker);
+// console.log(pops); 
+
+// Применяй .filter() для создания массива, содержащего только четные числа.
+// var numbers = [1, 2, 3, 4, 5];
+// var evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log(evenNumbers)
+
+// Используй .reduce() для получения общей суммы элементов массива.
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var allNumbers = numbers.reduce((sum, num) => sum + num, 0);
+// console.log(allNumbers)
+
+
+
+
+
+
+// function numbers(arr) {
+//   const suslik = arr.filter(num => num % 2 === 0);
+//   return suslik;
+// }
+// const result = [1, 2, 3, 5, 6, 7, 8, 92]; 
+// console.log(numbers(result))
+
+
+
+// function numb(arr) {
+//   const filter = arr.map(num => num * 10);
+//   return filter;
+// }
+// const result = [1, 2, 3, 5, 6, 7, 8, 92]; 
+// console.log(numb(result))
+
+
+
+// Напиши функцию, которая принимает строку и возвращает её в обратном порядке.
+
+
+// function reverseString(str) {
+//   let reversed = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// console.log(reverseString("hello")); 
+
